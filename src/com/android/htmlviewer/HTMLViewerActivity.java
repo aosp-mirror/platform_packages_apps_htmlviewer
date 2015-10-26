5e2fbb4039da7d12d1672cf5fb8f419d94da7945
Euisang Lim <eui-sang.lim@samsung.com>
2015-10-26 21:19:12 +0900

    Show runtime permission dialog in order to read storage
    In Marshmallow, HTMLViewer can not open text file
    because READ_EXTERNAL_STORAGE permission is off by default.
    In addition, it just shows up 'Webpage not available', so users
    can't notice that this is the permission problem. It would be better
    to show dialog to check permission for read storage in HTMLViewer.
    
    Change-Id: I26ad05136194cb36d4a17d3a8ab1a3fd2b5dfeb5
    Signed-off-by: Euisang Lim <eui-sang.lim@samsung.com>

--- src/com/android/htmlviewer/HTMLViewerActivity.java
+++ src/com/android/htmlviewer/HTMLViewerActivity.java
@@ -24,6 +24,7 @@ import android.content.pm.PackageManager;
 import android.Manifest;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -34,6 +35,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -49,7 +51,7 @@ public class HTMLViewerActivity extends Activity {
 
     private WebView mWebView;
     private View mLoading;
-    private Uri mOnPermissionDestination;
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,24 +79,31 @@ public class HTMLViewerActivity extends Activity {
         s.setJavaScriptEnabled(false);
         s.setDefaultTextEncodingName("utf-8");
 
-        final Intent intent = getIntent();
-        if (intent.hasExtra(Intent.EXTRA_TITLE)) {
-            setTitle(intent.getStringExtra(Intent.EXTRA_TITLE));
-        }
+        requestRuntimePermission();
+    }
 
-        Uri destination = intent.getData();
+    private void loadUrl() {
+        if (mIntent.hasExtra(Intent.EXTRA_TITLE)) {
+            setTitle(mIntent.getStringExtra(Intent.EXTRA_TITLE));
+        }
+        mWebView.loadUrl(String.valueOf(mIntent.getData()));
+    }
+
+    private void requestRuntimePermission() {
+        mIntent = getIntent();
+        Uri destination = mIntent.getData();
         if (destination != null) {
+            File extStorageDir = Environment.getExternalStorageDirectory();
             // Is this a local file?
-            if ("file".equals(destination.getScheme())) {
-                if (PackageManager.PERMISSION_DENIED ==
-                        checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    // If we don't have local file permissions, save the destination so we can try
-                    // again once they're granted.
-                    mOnPermissionDestination = destination;
-                    requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
-                }
+            if (destination.getPath().contains(extStorageDir.toString())
+                        && PackageManager.PERMISSION_DENIED ==
+                                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // If we don't have local file permissions, save the destination so we can try
+                // again once they're granted.
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            } else {
+                loadUrl();
             }
-            mWebView.loadUrl(destination.toString());
         }
     }
 
@@ -108,7 +117,11 @@ public class HTMLViewerActivity extends Activity {
 
         if (PackageManager.PERMISSION_GRANTED == grantResults[0]) {
             // Try again now that we have the permission.
-            mWebView.loadUrl(mOnPermissionDestination.toString());
+            loadUrl();
+        } else {
+            Toast.makeText(HTMLViewerActivity.this,
+                    R.string.turn_on_storage_permission, Toast.LENGTH_SHORT).show();
+            finish();
         }
     }
 
